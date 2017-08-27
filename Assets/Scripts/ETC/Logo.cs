8e9126d447116473916509c37778d1fb1e266a5e
ChanMob <sellingout@naver.com>
2017-08-27 17:37:39 +0900

    Add Logo Scene

---
+++ Assets/Scripts/ETC/Logo.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Logo : MonoBehaviour
+{
+    public Image[] img;
+
+    bool alphaOn;
+
+    float t = 0.0f;
+
+    void Start()
+    {
+        SubAlpha();
+    }
+
+	void Update ()
+    {
+        t += Time.deltaTime;
+
+        if(t > 1.0f)
+        {
+            alphaOn = !alphaOn;
+            t = 0.0f;
+        }
+
+        if(alphaOn)
+        {
+            AddAlpha();
+        }
+
+        else
+        {
+            SubAlpha();
+        }
+
+	    if(Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(1);
+        }	
+	}
+
+    void AddAlpha()
+    {
+        img[0].CrossFadeAlpha(1f, 1f, false);
+
+        for (int i = 1; i < img.Length; i++)
+        {
+            img[i].CrossFadeAlpha(1f, Random.Range(0.5f, 2.0f), false);
+        }
+    }
+
+    void SubAlpha()
+    {
+        img[0].CrossFadeAlpha(0.2f, 1f, false);
+
+        for (int i = 1; i < img.Length; i++)
+        {
+            img[i].CrossFadeAlpha(0.2f, Random.Range(0.5f, 2.0f), false);
+        }
+    }
+}
---
+++ Assets/Scripts/ETC/LogoAlpha.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogoAlpha : MonoBehaviour
+{
+    Image img;
+
+    float delayTime;
+    float checkTime;
+
+    bool alphaOn;
+
+	void Start ()
+    {
+        img = GetComponent<Image>();
+
+        delayTime = Random.Range(0.5f, 2.0f);
+	}
+	
+    void AddAlpha()
+    {
+        img.CrossFadeAlpha(1.0f, delayTime, true);
+
+        delayTime = Random.Range(0.5f, 2.0f);
+    }
+
+    void SubAlpha()
+    {
+        img.CrossFadeAlpha(0.0f, delayTime, true);
+
+        delayTime = Random.Range(0.5f, 2.0f);
+    }
+   
+	void Update ()
+    {
+        checkTime += Time.deltaTime;
+        
+        if(checkTime > delayTime)
+        {
+            if(alphaOn)
+            {
+                AddAlpha();
+            }
+
+            else
+            {
+                SubAlpha();
+            }
+
+            alphaOn = !alphaOn;
+
+            checkTime = 0;
+        }	
+	}
+}
