9b767fb4f1ad0c243bed20ac76489569be166f5a
SungJiYong <sjy9137@naver.com>
2017-09-14 21:58:52 +0900

    퍼즐 힌트 띄우기 기능 완료
    퍼즐 힌트 띄우기 기능 완료

--- Assets/Scripts/Puzzle/PuzzleEndManager.cs
+++ Assets/Scripts/Puzzle/PuzzleEndManager.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PuzzleEndManager : MonoBehaviour {
 	const int MaxPuzzle = 5;
 
+	public GameObject HintPanel;
+
+	private bool Hint = false;
 	public static bool PuzzleOn1 = false;
 	public static bool PuzzleOn2 = false;
 	public static bool PuzzleOn3 = false;
@@ -18,11 +22,11 @@ public class PuzzleEndManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		Debug.Log (PuzzleOn1);
+		/*Debug.Log (PuzzleOn1);
 		Debug.Log (PuzzleOn2);
 		Debug.Log (PuzzleOn3);
 		Debug.Log (PuzzleOn4);
-		Debug.Log (PuzzleOn5);
+		Debug.Log (PuzzleOn5);*/
 
 		if(PuzzleOn1 == true && PuzzleOn2 == true && PuzzleOn3 == true && PuzzleOn4 == true && PuzzleOn5 == true){
 
@@ -47,16 +51,30 @@ public class PuzzleEndManager : MonoBehaviour {
 		} else if (PuzzleEnd[0] == "Puzzle1" && PuzzleEnd[1] == "Puzzle2" &&  PuzzleEnd[2] == "Puzzle3" && PuzzleEnd[3] == "Puzzle4" && PuzzleEnd[4] == "Puzzle9") {
 			Debug.Log ("특전앤딩");
 		} else {	
-			GameObject.Find ("Puzzle1").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (500, -602, 0);
-			GameObject.Find ("Puzzle2").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (800, -602, 0);
-			GameObject.Find ("Puzzle3").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (1100, -602, 0);
-			GameObject.Find ("Puzzle4").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (-500, -602, 0);
-			GameObject.Find ("Puzzle5").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (344, -894, 0);
-			GameObject.Find ("Puzzle6").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (644, -894, 0);
-			GameObject.Find ("Puzzle7").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (944, -894, 0);
-			GameObject.Find ("Puzzle8").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (1244, -894, 0);
-			GameObject.Find ("Puzzle9").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (1544, -894, 0);
-			Debug.Log ("응 아니야 힌트");
+			HintPanel.SetActive (true);
+			this.gameObject.GetComponent<MovePuzzle> ().enabled = false;
+			//StopMovePuzzle ();
+			if (Hint == true) {
+				yield return new WaitForSeconds (0.5f);
+				GameObject.Find ("Puzzle1").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (500, -602, 0);
+				GameObject.Find ("Puzzle2").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (800, -602, 0);
+				GameObject.Find ("Puzzle3").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (1100, -602, 0);
+				GameObject.Find ("Puzzle4").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (-500, -602, 0);
+				GameObject.Find ("Puzzle5").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (344, -894, 0);
+				GameObject.Find ("Puzzle6").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (644, -894, 0);
+				GameObject.Find ("Puzzle7").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (944, -894, 0);
+				GameObject.Find ("Puzzle8").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (1244, -894, 0);
+				GameObject.Find ("Puzzle9").GetComponent<RectTransform> ().anchoredPosition = new Vector3 (1544, -894, 0);
+				Debug.Log ("응 아니야 힌트");
+				Hint = false;
+				this.gameObject.GetComponent<MovePuzzle> ().enabled = true;
+				HintPanel.SetActive (false);
+				StopCoroutine (ClearEndPuzzle());
+			}
 		}
+	}
+		
+	public void HintOn(){
+		Hint = true;
 	}
 }
