c53c202efacf8a17f0b18198436d8fddd775e0e6
ChanMob <sellingout@naver.com>
2017-10-08 10:29:45 +0900

    Add Object Mouse In Stage 1-3

--- Assets/Scripts/Object/MovingObj.cs
+++ Assets/Scripts/Object/MovingObj.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class MovingObj : MonoBehaviour {
 
+    Vector3 pos;
+
     public float leftmax;
     public float rightmax;
     public float speed;
 
     public bool isright;
+    public bool changeScale;
 
     public float angle;
+
+    private void Start()
+    {
+        pos = this.transform.localScale;
+    }
 
     void Update()
     {
@@ -21,10 +29,16 @@ public class MovingObj : MonoBehaviour {
         if(this.transform.position.x < leftmax)
         {
             isright = true;
+
+            if(changeScale)
+                this.transform.localScale = pos;
         }
         else if(this.transform.position.x > rightmax)
         {
             isright = false;
+
+            if(changeScale)
+                this.transform.localScale = new Vector3(-pos.x, pos.y, pos.z);
         }
 
         if (isright == true)
