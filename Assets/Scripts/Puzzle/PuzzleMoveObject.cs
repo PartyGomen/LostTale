d8341f20ca634bdcc06b374041b45a7ad2d07909
ChanMob <sellingout@naver.com>
2017-10-10 18:20:47 +0900

    Modified Hermit Crab In Stage 1-1

--- Assets/Scripts/Puzzle/PuzzleMoveObject.cs
+++ Assets/Scripts/Puzzle/PuzzleMoveObject.cs
@@ -28,27 +28,55 @@ public class PuzzleMoveObject : MonoBehaviour {
 	public bool DragOn4 = true;
 	public bool DragOn5 = true;
 
-
-
 	public GameObject Null;
 
-	// Update is called once per frame
-	void FixedUpdate () {
+    //추가
+    bool isDragging;
+    bool[] crabs = new bool[5];
 
-		if (Input.GetMouseButtonUp (0)) {
-			Ani1.SetBool ("IsOut", false);
-			Ani2.SetBool ("IsOut", false);
-			Ani3.SetBool ("IsOut", false);
-			Ani4.SetBool ("IsOut", false);
-			Ani5.SetBool ("IsOut", false);
-			DragOn1 = true;
-			DragOn2 = true;
-			DragOn3 = true;
-			DragOn4 = true;
-			DragOn5 = true;
-		}
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        CastRay();
 
-		/*if (Input.GetMouseButtonUp (0)) {
+    //        if (target.name == "1")
+    //        {
+    //            isDragging = true;
+    //            crabs[0] = true;
+    //        }
+
+    //        if (isDragging && crabs[0])
+    //        {
+    //            Crab1.transform.position = pos;
+    //        }
+    //    }
+
+    //    if (Input.GetMouseButtonUp(0))
+    //    {
+    //        isDragging = false;
+    //    }
+    //}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ani1.SetBool("IsOut", false);
+            Ani2.SetBool("IsOut", false);
+            Ani3.SetBool("IsOut", false);
+            Ani4.SetBool("IsOut", false);
+            Ani5.SetBool("IsOut", false);
+            DragOn1 = true;
+            DragOn2 = true;
+            DragOn3 = true;
+            DragOn4 = true;
+            DragOn5 = true;
+        }
+
+        /*if (Input.GetMouseButtonUp (0)) {
 			Ani1.SetBool ("IsOut", false);
 		}else if(this.gameObject.name.Equals("2")){
 			Ani2.SetBool ("IsOut", false);
@@ -60,48 +88,58 @@ public class PuzzleMoveObject : MonoBehaviour {
 			Ani5.SetBool ("IsOut", false);
 		}*/
 
-		if (Input.GetMouseButton (0)) {
-			CastRay ();	
-			if(target.name== "1" && DragOn1 == true) {
-				Ani1.SetBool ("IsOut", true);
-				Crab1.transform.position = pos;
-				DragOn2 = false;
-				DragOn3 = false;
-				DragOn4 = false;
-				DragOn5 = false;
-			}else if(target.name == "2" && DragOn2 == true){
-				Ani2.SetBool ("IsOut", true);
-				Crab2.transform.position = pos;
-				DragOn1 = false;
-				DragOn3 = false;
-				DragOn4 = false;
-				DragOn5 = false;
-			}else if(target.name == "3" && DragOn3 == true){
-				Ani3.SetBool ("IsOut", true);
-				Crab3.transform.position = pos;
-				DragOn1 = false;
-				DragOn2 = false;
-				DragOn4 = false;
-				DragOn5 = false;
-			}else if(target.name == "4" && DragOn4 == true){
-				Ani4.SetBool ("IsOut", true);
-				Crab4.transform.position = pos;
-				DragOn1 = false;
-				DragOn2 = false;
-				DragOn3 = false;
-				DragOn5 = false;
-			}else if(target.name == "5" && DragOn5 == true){
-				Ani5.SetBool ("IsOut", true);
-				Crab5.transform.position = pos;
-				DragOn1 = false;
-				DragOn2 = false;
-				DragOn3 = false;
-				DragOn4 = false;
-			}
-		}
-	}
-		
-	void CastRay(){
+        if (Input.GetMouseButton(0))
+        {
+            CastRay();
+            if (target.name == "1" && DragOn1 == true)
+            {
+                Ani1.SetBool("IsOut", true);
+                Crab1.transform.position = pos;
+                DragOn2 = false;
+                DragOn3 = false;
+                DragOn4 = false;
+                DragOn5 = false;
+            }
+            else if (target.name == "2" && DragOn2 == true)
+            {
+                Ani2.SetBool("IsOut", true);
+                Crab2.transform.position = pos;
+                DragOn1 = false;
+                DragOn3 = false;
+                DragOn4 = false;
+                DragOn5 = false;
+            }
+            else if (target.name == "3" && DragOn3 == true)
+            {
+                Ani3.SetBool("IsOut", true);
+                Crab3.transform.position = pos;
+                DragOn1 = false;
+                DragOn2 = false;
+                DragOn4 = false;
+                DragOn5 = false;
+            }
+            else if (target.name == "4" && DragOn4 == true)
+            {
+                Ani4.SetBool("IsOut", true);
+                Crab4.transform.position = pos;
+                DragOn1 = false;
+                DragOn2 = false;
+                DragOn3 = false;
+                DragOn5 = false;
+            }
+            else if (target.name == "5" && DragOn5 == true)
+            {
+                Ani5.SetBool("IsOut", true);
+                Crab5.transform.position = pos;
+                DragOn1 = false;
+                DragOn2 = false;
+                DragOn3 = false;
+                DragOn4 = false;
+            }
+        }
+    }
+
+    void CastRay(){
 		target = null;
 		pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		RaycastHit2D hit = Physics2D.Raycast (pos, Vector2.zero, 0f);
