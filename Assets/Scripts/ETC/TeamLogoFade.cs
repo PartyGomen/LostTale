eaf8bda86e6873a2235149b60c816b832b6d5feb
ChanMob <sellingout@naver.com>
2017-11-12 13:27:16 +0900

    Add TeamLogo Scene

---
+++ Assets/Scripts/ETC/TeamLogoFade.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeamLogoFade : MonoBehaviour
+{
+    enum FADE_OBJECT
+    {
+        IMAGE,
+        FONT,
+        ALLFADE,
+        NONE
+    };
+
+    public float[] fade_time = null;
+
+    public Image[] ui_object = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(AllFadeIn());
+        StartCoroutine(ImgFadeOut());
+        StartCoroutine(FontFadeOut());
+        StartCoroutine(AllFadeOut());
+	}
+
+    IEnumerator AllFadeOut()
+    {
+        yield return new WaitForSeconds(5f);
+
+        float t = 0.0f;
+
+        while (t <= 1)
+        {
+            t += Time.deltaTime / fade_time[(int)FADE_OBJECT.ALLFADE];
+
+            ui_object[(int)FADE_OBJECT.ALLFADE].color = new Color(0, 0, 0, t);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator AllFadeIn()
+    {
+        float t = 1.0f;
+
+        while (t >= 0)
+        {
+            t -= Time.deltaTime / fade_time[(int)FADE_OBJECT.ALLFADE];
+
+            ui_object[(int)FADE_OBJECT.ALLFADE].color = new Color(0, 0, 0, t);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator ImgFadeOut()
+    {
+        yield return new WaitForSeconds(1f);
+
+        float t = 0.0f;
+
+        while (t >= 0)
+        {
+            t += Time.deltaTime / fade_time[(int)FADE_OBJECT.IMAGE];
+
+            ui_object[(int)FADE_OBJECT.IMAGE].color = new Color(1, 1, 1, t);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator FontFadeOut()
+    {
+        yield return new WaitForSeconds(2f);
+
+        float t = 0.0f;
+
+        while (t >= 0)
+        {
+            t += Time.deltaTime / fade_time[(int)FADE_OBJECT.FONT];
+
+            ui_object[(int)FADE_OBJECT.FONT].color = new Color(1, 1, 1, t);
+
+            yield return null;
+        }
+    }
+}
