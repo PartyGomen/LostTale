bcc9944cf75756b75302191a452314c033177931
SungJiYong <sjy9137@naver.com>
2017-11-16 01:35:39 +0900

    갤러리 타이틀 이미지 추가 및 해금 기능 완료
    갤러리 타이틀 이미지 추가 및 해금 기능 완료

--- Assets/GalleryManager.cs
+++ Assets/GalleryManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GalleryManager : MonoBehaviour {
 
@@ -9,13 +10,32 @@ public class GalleryManager : MonoBehaviour {
 	public static bool TrueEnding = false;
 	public static bool SpecialEnding = false;
 
+	public GameObject[] NowTitle;
+	public Sprite[] TitleImage;
+	public GameObject[] DisableImage;
+	public GameObject[] NotImage;
 	// Use this for initialization
-	void Start () {
-		
+
+	void Start(){
+		if(TrueEnding == true){
+			CheckGallery (0);
+		}else if(SadEnding == true){
+			CheckGallery (1);
+		}else if(HappyEnding == true){
+			CheckGallery (2);
+		}else if(SpecialEnding == true){
+			CheckGallery (3);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void CheckGallery(int number){
+		NowTitle [number].GetComponent<Image> ().sprite = TitleImage [3];
+		DisableImage [number].SetActive (false);
+		NotImage[number].SetActive (false);
 	}
 }
