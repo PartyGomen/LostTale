d30a88d815721d5ffdce4ed189b18297747cb9f9
SungJiYong <sjy9137@naver.com>
2017-11-18 16:08:29 +0900

    파티클 샘플 완성 (오브젝트에 미적용)
    파티클 샘플 완성 (오브젝트에 미적용)

--- Assets/Scripts/ETC/Tutorial.cs
+++ Assets/Scripts/ETC/Tutorial.cs
@@ -53,49 +53,39 @@ public class Tutorial : MonoBehaviour
     {
         switch (type)
         {
-            case TUTORIAL_TYPE.PADDLE:
-                {
-                    if(bridge.is_clear == false)
-                    {
-                        distance = Vector3.Distance(player.gameObject.transform.position, bridge.gameObject.transform.position);
+		case TUTORIAL_TYPE.PADDLE:
+			{
+				if (bridge.is_clear == false) {
+					distance = Vector3.Distance (player.gameObject.transform.position, bridge.gameObject.transform.position);
 
-                        if (distance < 7)
-                        {
-                            GetComponent<SpriteRenderer>().sprite = image_sprite[(int)IMAGE_TYPE.SMALL_HAND];
-                            time += Time.deltaTime;
+					if (distance < 7) {
+						GetComponent<SpriteRenderer> ().sprite = image_sprite [(int)IMAGE_TYPE.SMALL_HAND];
+						time += Time.deltaTime;
 
-                            if(time > 0.5f)
-                            {
-                                transform.transform.eulerAngles = new Vector3(0, 0, 0);
-                                transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
-                                transform.Translate(Vector2.left * Time.deltaTime * 2);
+						if (time > 0.5f) {
+							transform.transform.eulerAngles = new Vector3 (0, 0, 0);
+							transform.localScale = new Vector3 (0.8f, 0.8f, 0.8f);
+							transform.Translate (Vector2.left * Time.deltaTime * 2);
 
-                                if (transform.position.x < -2f)
-                                {
-                                    time = 0.0f;
-                                    this.transform.position = new Vector3(0.6f, 4.5f, 0);
-                                    this.transform.eulerAngles = new Vector3(0, 0, -30);
-                                    transform.localScale = new Vector3(1f, 1f, 1f);
-                                }
-                            }
-
-                        else
-                        {
-                            time = 0.0f;
-                            GetComponent<SpriteRenderer>().sprite = image_sprite[(int)IMAGE_TYPE.NONE];
-                            this.transform.position = new Vector3(0.6f, 4.5f, 0);
-                            this.transform.eulerAngles = new Vector3(0, 0, -30);
-                            transform.localScale = new Vector3(1f, 1f, 1f);
-                        }
-                    }
-
-                    else
-                    {
-                        this.gameObject.SetActive(false);
-                    }
-                }
-                break;
-
+							if (transform.position.x < -2f) {
+								time = 0.0f;
+								this.transform.position = new Vector3 (0.6f, 4.5f, 0);
+								this.transform.eulerAngles = new Vector3 (0, 0, -30);
+								transform.localScale = new Vector3 (1f, 1f, 1f);
+							}
+						} else {
+							time = 0.0f;
+							GetComponent<SpriteRenderer> ().sprite = image_sprite [(int)IMAGE_TYPE.NONE];
+							this.transform.position = new Vector3 (0.6f, 4.5f, 0);
+							this.transform.eulerAngles = new Vector3 (0, 0, -30);
+							transform.localScale = new Vector3 (1f, 1f, 1f);
+						}
+					} else {
+						this.gameObject.SetActive (false);
+					}
+				}
+				break;
+			}
             case TUTORIAL_TYPE.TOUCH:
                 {
 
