4f3d225930aec7b10b6a88c28f52cf40b9e486b4
ChanMob <sellingout@naver.com>
2017-11-22 18:01:46 +0900

    Add Piano Puzzle But Not Complete

---
+++ Assets/Scripts/Puzzle/PianoKey.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PianoKey : MonoBehaviour
+{
+    public string key;
+
+    SpriteRenderer sprite;
+
+    public GameObject[] notes;
+
+    int random_value;
+
+    float time;
+    float random_x;
+    float random_y;
+
+    RaycastHit2D hit;
+
+    Vector3 mouse_pos;
+    Vector3 random_vector;
+
+    Transform random_transform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            hit = Physics2D.Raycast(mouse_pos, Vector2.zero, Mathf.Infinity);
+
+            if(hit)
+            {
+                if(hit.collider.gameObject == this.gameObject)
+                {
+                    sprite.color = new Color32(200, 200, 200, 255);
+                    Invoke("ColorBack", 0.2f);
+                    //수정해야함
+                    //random_value = Random.Range(0, notes.Length);
+                    //random_x = Random.Range(20.0f, 40.0f);
+                    //random_y = Random.Range(-25.0f, -35.0f);
+                    //random_vector = new Vector3(random_x, random_y, 0);
+                    //random_transform.position = random_vector;
+                    //Instantiate(notes[random_value], random_transform);
+                }
+            }
+        }
+	}
+
+    void ColorBack()
+    {
+        sprite.color = new Color(1, 1, 1, 1);
+    }
+
+    bool IsPointerOverUIObject()
+    {
+        // Referencing this code for GraphicRaycaster 
+        // https://gist.github.com/stramit/ead7ca1f432f3c0f181f
+        // the ray cast appears to require only eventData.position.
+        PointerEventData eventDataCurrentPosition
+            = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position
+            = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+}
