6bb54dc2124ded101e0899f2f0d7a69dc28a1f26
ChanMob <sellingout@naver.com>
2017-11-26 14:22:51 +0900

    Modified Tutorial

--- Assets/Scripts/ETC/Tutorial.cs
+++ Assets/Scripts/ETC/Tutorial.cs
@@ -13,33 +13,28 @@ public class Tutorial : MonoBehaviour
 		NONE
 	}
 
-	public enum IMAGE_TYPE
-	{
-		HAND,
-		SMALL_HAND,
-		DOT,
-		ARROW_LEFT,
-		NONE
-	}
-
-	public Sprite[] image_sprite;
-
 	public TUTORIAL_TYPE type = TUTORIAL_TYPE.NONE;
 
 	private Player player;
 	private Bridge bridge;
     private PuzzleShow puzzle_show;
+    private Vector3 pos;
+    public GameObject[] tutorial_image;
 
 	public float distance;
-	float time;
+    public float min_value;
+	private float time;
+    private float alpha;
 
     [HideInInspector]
     public bool is_clicked;
+    private bool is_first;
 
 	// Use this for initialization
 	void Start ()
 	{
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        pos = this.transform.position;
 
 		switch (type)
         {
@@ -60,51 +55,76 @@ public class Tutorial : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+        alpha = Mathf.Clamp(alpha, 0.0f, 1.0f);
+
 		switch (type)
 		{
-		case TUTORIAL_TYPE.DRAG:
-			{
-				if(bridge.is_clear == false)
-				{
-					distance = Vector3.Distance(player.gameObject.transform.position, bridge.gameObject.transform.position);
+            case TUTORIAL_TYPE.DRAG:
+                {
+                    GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
 
-					if (distance < 7)
-					{
-						GetComponent<SpriteRenderer>().sprite = image_sprite[(int)IMAGE_TYPE.SMALL_HAND];
-						time += Time.deltaTime;
+                    for (int i = 0; i < tutorial_image.Length; i++)
+                    {
+                        tutorial_image[i].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
+                    }
 
-						if (time > 0.5f)
-						{
-							transform.transform.eulerAngles = new Vector3(0, 0, 0);
-							transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
-							transform.Translate(Vector2.left * Time.deltaTime * 2);
+                    if (bridge.is_clear == false)
+                    {
+                        distance = Vector3.Distance(player.gameObject.transform.position, bridge.gameObject.transform.position);
 
-							if (transform.position.x < -2f)
-							{
-								time = 0.0f;
-								this.transform.position = new Vector3(0.6f, 4.5f, 0);
-								this.transform.eulerAngles = new Vector3(0, 0, -30);
-								transform.localScale = new Vector3(1f, 1f, 1f);
-							}
-						}
-					}
+                        if (distance < 7)
+                        {
+                            if(!is_first)
+                            {
+                                time = 0.0f;
+                                this.transform.position = pos;
+                                this.transform.eulerAngles = new Vector3(0, 0, -30);
+                                transform.localScale = new Vector3(1f, 1f, 1f);
+                            }
 
-					else
-					{
-						time = 0.0f;
-						GetComponent<SpriteRenderer>().sprite = image_sprite[(int)IMAGE_TYPE.NONE];
-						this.transform.position = new Vector3(0.6f, 4.5f, 0);
-						this.transform.eulerAngles = new Vector3(0, 0, -30);
-						transform.localScale = new Vector3(1f, 1f, 1f);
-					}
-				}
+                            is_first = true;
 
-				else
-				{
-					this.gameObject.SetActive(false);
-				}
-			}
-			break;
+                            alpha += Time.deltaTime;
+                            time += Time.deltaTime;
+
+                            if (time > 0.5f)
+                            {
+                                transform.transform.eulerAngles = new Vector3(0, 0, 0);
+                                transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+                                transform.Translate(Vector2.left * Time.deltaTime * 2);
+
+                                if (transform.position.x < min_value)
+                                {
+                                    time = 0.0f;
+                                    this.transform.position = pos;
+                                    this.transform.eulerAngles = new Vector3(0, 0, -30);
+                                    transform.localScale = new Vector3(1f, 1f, 1f);
+                                }
+                            }
+                        }
+
+                        else
+                        {
+                            alpha -= Time.deltaTime;
+                            is_first = false;
+                        }
+                    }
+
+                    else
+                    {
+                        alpha -= Time.deltaTime;
+
+                        if(alpha <= 0)
+                        {
+                            for (int i = 0; i < tutorial_image.Length; i++)
+                            {
+                                tutorial_image[i].SetActive(false);
+                            }
+                            this.gameObject.SetActive(false);
+                        }
+                    }
+                }
+                break;
 
             case TUTORIAL_TYPE.TOUCH:
                 {
@@ -112,9 +132,23 @@ public class Tutorial : MonoBehaviour
                     {
                         distance = Vector3.Distance(player.gameObject.transform.position, puzzle_show.gameObject.transform.position);
 
-                        if(distance < 5f)
+                        if (distance < 5f)
                         {
-                            GetComponent<SpriteRenderer>().sprite = image_sprite[(int)IMAGE_TYPE.SMALL_HAND];
+                            if (!is_first)
+                            {
+                                time = 0.0f;
+                                this.transform.eulerAngles = new Vector3(0, 0, -30);
+                                transform.localScale = new Vector3(1f, 1f, 1f);
+                            }
+
+                            is_first = true;
+
+                            alpha += Time.deltaTime;
+                            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
+                            for (int i = 0; i < tutorial_image.Length; i++)
+                            {
+                                tutorial_image[i].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
+                            }
                             time += Time.deltaTime;
 
                             if (time > 0.5f)
@@ -133,10 +167,31 @@ public class Tutorial : MonoBehaviour
 
                         else
                         {
-                            time = 0.0f;
-                            GetComponent<SpriteRenderer>().sprite = image_sprite[(int)IMAGE_TYPE.NONE];
-                            this.transform.eulerAngles = new Vector3(0, 0, -30);
-                            transform.localScale = new Vector3(1f, 1f, 1f);
+                            alpha -= Time.deltaTime;
+                            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
+                            for (int i = 0; i < tutorial_image.Length; i++)
+                            {
+                                tutorial_image[i].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
+                            }
+                            is_first = false;
+                        }
+                    }
+                    else
+                    {
+                        alpha -= Time.deltaTime;
+                        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
+                        for (int i = 0; i < tutorial_image.Length; i++)
+                        {
+                            tutorial_image[i].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
+                        }
+
+                        if (alpha <= 0)
+                        {
+                            for (int i = 0; i < tutorial_image.Length; i++)
+                            {
+                                tutorial_image[i].SetActive(false);
+                            }
+                            this.gameObject.SetActive(false);
                         }
                     }
                 }
@@ -150,9 +205,9 @@ public class Tutorial : MonoBehaviour
 
             case TUTORIAL_TYPE.BULB:
                 {
-                    if(!is_clicked)
+                    if (!is_clicked)
                     {
-                        GetComponent<SpriteRenderer>().sprite = image_sprite[(int)IMAGE_TYPE.SMALL_HAND];
+                        GetComponent<SpriteRenderer>().enabled = true;
                         transform.Translate(Vector2.up * Time.deltaTime * 0.7f);
 
                         if (transform.position.y < -33f)
@@ -168,5 +223,5 @@ public class Tutorial : MonoBehaviour
                 }
                 break;
         }
-	}
+    }
 }
