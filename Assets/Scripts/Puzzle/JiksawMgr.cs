fbafce3235c63db95de1231e624c4fbb67ee1885
ChanMob <sellingout@naver.com>
2017-12-03 20:52:43 +0900

    Have To Fix Jiksaw Puzzle

--- Assets/Scripts/Puzzle/JiksawMgr.cs
+++ Assets/Scripts/Puzzle/JiksawMgr.cs
@@ -6,6 +6,8 @@ public class JiksawMgr : MonoBehaviour
 {
     public Jiksaw[] jiksawObj;
 
+    public float move_value;
+
     Vector3 pos;
     RaycastHit2D hit;
 
@@ -18,19 +20,19 @@ public class JiksawMgr : MonoBehaviour
             hit = Physics2D.Raycast(pos, Vector2.zero, Mathf.Infinity);
 
             if (hit)
-            {
-                if (hit.collider.gameObject.name == "JikSaw")
+            { 
+                if (hit.collider.gameObject.tag == "JikSaw")
                 {
                     Jiksaw jik = hit.collider.gameObject.GetComponent<Jiksaw>();
 
                     if (!jik.move[0])
-                        hit.collider.gameObject.transform.Translate(Vector2.right * 1);
+                        hit.collider.gameObject.transform.Translate(Vector2.right * move_value);
                     else if (!jik.move[1])
-                        hit.collider.gameObject.transform.Translate(Vector2.left * 1);
+                        hit.collider.gameObject.transform.Translate(Vector2.left * move_value);
                     else if (!jik.move[2])
-                        hit.collider.gameObject.transform.Translate(Vector2.up * 1);
+                        hit.collider.gameObject.transform.Translate(Vector2.up * move_value);
                     else if (!jik.move[3])
-                        hit.collider.gameObject.transform.Translate(Vector2.down * 1);
+                        hit.collider.gameObject.transform.Translate(Vector2.down * move_value);
 
                     CheckBool();
                 }
