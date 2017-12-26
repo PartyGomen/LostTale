b2cbd8245f6a0cfa6ce75900924f90a280fadc87
DESKTOP-0RBE0GM\chanm <sellingout@naver.com>
2017-12-26 23:38:54 +0900

    게임 튜토리얼 작업중

---
+++ Assets/Scripts/ETC/GameTutorial.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTutorial : MonoBehaviour
+{
+    public Player[] player = new Player[2];
+
+    private float[] time_check = new float[2];
+
+	// Update is called once per frame
+	void Update ()
+    {
+        time_check[0] += Time.deltaTime;
+
+        if (time_check[0] > 1.0f)
+        {
+            time_check[0] = 0.0f;
+
+            time_check[1] += Time.deltaTime;
+
+            if (time_check[1] < 1f)
+            {
+                for(int i = 0; i < 2; i++)
+                {
+                    player[i].Button_Jump_press();
+                }
+            }
+
+            else
+            {
+                time_check[1] = 0.0f;
+            }
+        }
+
+        else
+        {
+            player[0].Button_Jump_release();
+        }
+    }
+}
---
+++ Assets/Scripts/ETC/Tutorial_Hand.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_Hand : MonoBehaviour
+{
+    public GameObject[] hand_image = new GameObject[2];
+    private float[] time_check = new float[3];
+    private bool[] change = new bool[2];
+
+	void Update ()
+    {
+        time_check[0] += Time.deltaTime;
+
+        if(time_check[0] > 1f)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                change[i] = true;
+
+                ChangeHandImage(i, true);
+            }
+
+            time_check[0] = 0.0f;
+        }
+
+        if(change[0] == true)
+        {
+            time_check[1] += Time.deltaTime;
+
+            if(time_check[1] >= 0.2f)
+            {
+                change[0] = false;
+                ChangeHandImage(0, false);
+                time_check[1] = 0.0f;
+            }
+        }
+
+        if (change[1] == true)
+        {
+            time_check[2] += Time.deltaTime;
+
+            if (time_check[2] >= 0.5f)
+            {
+                change[1] = false;
+                ChangeHandImage(1, false);
+                time_check[2] = 0.0f;
+            }
+        }
+    }
+
+    void ChangeHandImage(int _idx, bool _true)
+    {
+        if(_true)
+        {
+            hand_image[_idx].transform.eulerAngles = new Vector3(0, 0, 0);
+            hand_image[_idx].transform.localScale = new Vector3(0.8f, 0.8f, 1);
+        }
+
+        else
+        {
+            hand_image[_idx].transform.eulerAngles = new Vector3(0, 0, -30);
+            hand_image[_idx].transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
