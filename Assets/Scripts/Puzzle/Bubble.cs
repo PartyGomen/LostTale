572b88e0d8787fe3b8263db032431a4eeee9e8db
DESKTOP-0RBE0GM\chanm <sellingout@naver.com>
2017-12-26 19:25:13 +0900

    버블 추가

--- Assets/Scripts/Puzzle/Bubble.cs
+++ Assets/Scripts/Puzzle/Bubble.cs
@@ -12,6 +12,7 @@ public class Bubble : MonoBehaviour
     private float vector_x;
     private float alpha_value;
     private float divide_value;
+    private float scale_value;
     public float vector_y;
 
 	// Use this for initialization
@@ -19,6 +20,7 @@ public class Bubble : MonoBehaviour
     {
         sprite = GetComponent<SpriteRenderer>();
         start_pos = this.transform.position;
+        RandomValue();
 	}
 	
 	// Update is called once per frame
@@ -27,11 +29,13 @@ public class Bubble : MonoBehaviour
         if (alpha_value <= 0)
         {
             RandomValue();
-            this.transform.position = new Vector3(vector_x, vector_y, 0);
+            this.transform.position = new Vector3(start_pos.x, vector_y, 0);
         }
 
         else
         {
+            this.transform.localScale = new Vector3(scale_value, scale_value, 0);
+            scale_value += Time.deltaTime / divide_value;
             alpha_value -= Time.deltaTime / divide_value;
             transform.Translate(Vector2.up * Time.deltaTime * speed);
             sprite.color = new Color(1, 1, 1, alpha_value);
@@ -40,9 +44,10 @@ public class Bubble : MonoBehaviour
 
     void RandomValue()
     {
+        scale_value = 0.1f;
         alpha_value = 1.0f; 
-        vector_x = Random.Range(start_pos.x - 0.2f, start_pos.x + 0.2f);
-        speed = Random.Range(0.5f, 1.0f);
+        //vector_x = Random.Range(start_pos.x - 0.2f, start_pos.x + 0.2f);
+        speed = Random.Range(0.4f, 0.8f);
         divide_value = Random.Range(1.0f, 2.0f);
     }
 }
