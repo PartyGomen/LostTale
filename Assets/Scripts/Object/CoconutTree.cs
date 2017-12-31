6d1488f4420a18f001223eb05ae440a68ca370bd
DESKTOP-0RBE0GM\chanm <sellingout@naver.com>
2017-12-31 16:22:49 +0900

    페이드 시키는 panel 크기 수정 / coconut 스크립트 수정

--- Assets/Scripts/Object/CoconutTree.cs
+++ Assets/Scripts/Object/CoconutTree.cs
@@ -6,11 +6,14 @@ using UnityEngine.EventSystems;
 public class CoconutTree : MonoBehaviour
 {
     Vector3 objPos;
-    Vector3 pos;
-    Vector3 offset;
 
     public float minY;
     public float maxY;
+    public float multiply;
+
+    private float diff;
+    private float end_pos;
+    private float start_pos;
 
     bool isDragging;
 	
@@ -19,20 +22,19 @@ public class CoconutTree : MonoBehaviour
     {
         if (!IsPointerOverUIObject())
         {
-
             if (Input.GetMouseButtonDown(0))
             {
-                pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                objPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-                RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, Mathf.Infinity);
+                RaycastHit2D hit = Physics2D.Raycast(objPos, Vector2.zero, Mathf.Infinity);
 
                 if (hit == true)
                 {
                     if (hit.collider.gameObject == this.gameObject)
                     {
-                        offset = this.transform.position - pos;
-
                         isDragging = true;
+
+                        start_pos = this.transform.position.y;
                     }
                 }
             }
@@ -40,14 +42,26 @@ public class CoconutTree : MonoBehaviour
             if (Input.GetMouseButton(0) && isDragging == true)
             {
                 objPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                
-                objPos.x = this.transform.position.x;
-                objPos.y += offset.y;
-                objPos.y = Mathf.Clamp(objPos.y, minY, maxY);
-                objPos.z = 0;
 
-                this.transform.position = objPos;
-				this.GetComponent<AudioSource> ().Play ();
+                objPos = new Vector3(objPos.x, Mathf.Clamp(objPos.y, minY, maxY), 0);
+
+                diff = this.transform.position.y - objPos.y;
+
+                transform.Translate(Vector2.down * diff * multiply * Time.deltaTime);
+
+                this.transform.position = new Vector3(this.transform.position.x, Mathf.Clamp(this.transform.position.y, minY, maxY), this.transform.position.z);
+
+                end_pos = this.transform.position.y;
+
+                if (0.5f < Mathf.Abs(start_pos - end_pos))
+                {
+                    if (GetComponent<AudioSource>().isPlaying == false)
+                    {
+                        GetComponent<AudioSource>().Play();
+
+                        start_pos = this.transform.position.y;
+                    }
+                }
             }
 
 			if (Input.GetMouseButtonUp (0)) 
